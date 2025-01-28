10453629590167c0c07250338c77157b5d82f6bd
sergiocastro85 <sergiocastro86@gmail.com>
2025-01-28 18:15:27 -0500

    Inital proyect load

---
+++ Application/UseCases/Products/DeleteProduct.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Products
+{
+    public class DeleteProduct
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProduct(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void Execute(int id)
+        {
+            _productRepository.DeleteProduct(id);
+        }
+    }
+}
---
+++ Application/UseCases/Products/GetAllProducts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.UseCases.Products
+{
+    public class GetAllProducts
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetAllProducts(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IEnumerable<Product> Execute()
+        {
+            return _productRepository.GetProducts();
+        }
+    }
+}
